c83d09062d1b2838aad55e01e18dd40bd64ac477
Tom <tom@ifold.cf>
2022-07-28 22:34:31 +0800

    oauth_user = OAuthGitHubUser.objects.get(openid=github_user_info['id']) github_user = oauth_user.user

--- meiduo_mall/meiduo_mall/apps/oauth/views.py
+++ meiduo_mall/meiduo_mall/apps/oauth/views.py
@@ -1,13 +1,17 @@
 from django.conf import settings
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 from django.views import View
 from django.http import JsonResponse, HttpResponse
 
 from meiduo_mall.utils.response_code import RETCODE
 from meiduo_mall.utils.github_oauth import OAuthGitHub
+from meiduo_mall.apps.oauth.models import OAuthGitHubUser
 
 
 # Create your views here.
+
+
 class GitHubOAuthURLView(View):
     def get(self, request):
         next_ = request.GET.get('next')
@@ -25,4 +29,13 @@ class GitHubOAuthView(View):
         github.get_github_access_token()
         github_user_info = github.get_github_user_info()
         github_user_info['netx'] = next_
-        return HttpResponse(str(github_user_info))
+        try:
+            oauth_user = OAuthGitHubUser.objects.get(openid=github_user_info['id'])
+        except OAuthGitHubUser.DoesNotExist:
+            pass
+        else:
+            github_user = oauth_user.user
+            login(request, github_user)
+            response = redirect(next_)
+            response.set_cookie('username', github_user.username, max_age=3600 * 24 * 15)
+        return response
