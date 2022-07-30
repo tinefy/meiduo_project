3f4623742344065117001dc5bc2bc22f1309f11f
Tom <tom@ifold.cf>
2022-07-31 04:14:45 +0800

    ''

--- meiduo_mall/meiduo_mall/utils/signer.py
+++ meiduo_mall/meiduo_mall/utils/signer.py
@@ -1,39 +1,13 @@
-from django.conf import settings
 import os
+import time
 
-from django.core.signing import Signer, TimestampSigner
-
-
-class SignData(object):
-    def sign(self, data, key=None, sep=':', salt=None, time=False):
-        # settings.SECRET_KEY
-        if not time:
-            singer = Signer(key=key, sep=sep, salt=salt)
-            data_signed = singer.sign(data)
-        else:
-            singer = TimestampSigner(key=key, sep=sep, salt=salt)
-            data_signed = singer.sign(data)
-        return data_signed
-
-    def unsign(self, data, key=None, sep=':', salt=None, time=None):
-        if not time:
-            singer = Signer(key=key, sep=sep, salt=salt)
-            data_unsigned = singer.unsign(data)
-        else:
-            singer = TimestampSigner(key=key, sep=sep, salt=salt)
-            data_unsigned = singer.unsign(data, time)
-        return data_unsigned
-
+from django.core import signing
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meiduo_mall.meiduo_mall.settings.dev')
-    singer = SignData()
-    data = singer.sign('abc', key=settings.SECRET_KEY,salt='1')
+    data = 'abc'
+    data = signing.dumps(data)
     print(data)
-    data = singer.sign('abc', key=settings.SECRET_KEY,salt='1')
+    time.sleep(6)
+    data = signing.loads(data, max_age=5)
     print(data)
-    # data=data.split(':')[1]
-    # print(data)
-    # data=singer.unsign(data)
-    # print(data)
-
