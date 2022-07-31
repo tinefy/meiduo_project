3ec30dcfcf263aac3498fc67e026b4cb52726c41
Tom <tom@ifold.cf>
2022-07-31 15:12:06 +0800

    delete username input label

--- meiduo_mall/meiduo_mall/static/js/oauth_callback.js
+++ meiduo_mall/meiduo_mall/static/js/oauth_callback.js
@@ -2,7 +2,6 @@ let vm = new Vue({
     el: '#app',
     delimiters: ['[[', ']]'],
     data: {
-        username: 'mduser',
         password: 'mduser123',
         password2: 'mduser123',
         mobile: '18674115441',
@@ -21,46 +20,18 @@ let vm = new Vue({
         sending_flag: false,
         sms_code_tip: '获取短信验证码',
 
-        error_name: false,
         error_password: false,
         error_password2: false,
         error_mobile: false,
         error_allow: false,
         error_submit: false,
 
-        error_name_message: '',
         error_mobile_message: '',
     },
     mounted: function () {
         this.generate_image_code();
     },
     methods: {
-        check_username: function () {
-            let re = /^[\w\d_-]{5,20}$/;
-            if (re.test(this.username)) {
-                this.error_name = false;
-            } else {
-                this.error_name = true;
-                this.error_name_message = '请输入5-20个字符的用户名';
-            }
-            let url = '/usernames/' + this.username + '/count/';
-            axios.get(
-                url, {responseType: 'json'}
-            ).then(
-                response => {
-                    if (response.data.count == 1) {
-                        this.error_name_message = '用户名已存在！';
-                        this.error_name = true;
-                    } else {
-                        this.error_name = false;
-                    }
-                }
-            ).catch(
-                error => {
-                    console.log(error.response)
-                }
-            )
-        },
         check_password: function () {
             let re = /^[\w\d]{8,20}$/;
             if (re.test(this.password)) {
@@ -90,11 +61,9 @@ let vm = new Vue({
             ).then(
                 response => {
                     if (response.data.count == 1) {
-                        this.error_mobile_message = '手机号已存在！';
-                        this.error_mobile = true;
-                    } else {
-                        this.error_mobile = false;
+                        this.error_mobile_message = '手机号已存在，将绑定此帐号！';
                     }
+                    this.error_mobile = false;
                 }
             ).catch(
                 error => {
@@ -214,12 +183,11 @@ let vm = new Vue({
             }
         },
         on_submit: function (event) {
-            this.check_username();
             this.check_password();
             this.check_password2();
             this.check_mobile();
             this.check_allow();
-            if (this.error_name == true || this.error_password == true || this.error_password2 == true
+            if (this.error_password == true || this.error_password2 == true
                 || this.error_mobile == true || this.error_allow == true) {
                 this.error_submit = true;
                 event.preventDefault();
