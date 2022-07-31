afc47156bfbc93b5e2c990be43ace789699df5ce
Tom <tom@ifold.cf>
2022-07-31 14:58:54 +0800

    'oauth_callback.html'

--- meiduo_mall/meiduo_mall/static/js/oauth_callback.js
+++ meiduo_mall/meiduo_mall/static/js/oauth_callback.js
@@ -1,136 +1,229 @@
 let vm = new Vue({
-	el: '#app',
+    el: '#app',
     delimiters: ['[[', ']]'],
-	data: {
-		mobile: '',
-		password: '',
-		image_code: '',
-		sms_code: '',
+    data: {
+        username: 'mduser',
+        password: 'mduser123',
+        password2: 'mduser123',
+        mobile: '18674115441',
+        allow: 'true',
 
-		error_mobile: false,
-		error_password: false,
-		error_image_code: false,
-		error_sms_code: false,
+        uuid: '',
+        image_code_url: '',
+        image_code_text: '',
+        image_code: '',
+        error_image_code: false,
+        error_image_code_message: '',
 
-		error_mobile_message: '',
-		error_image_code_message: '',
-		error_sms_code_message: '',
+        sms_code: '',
+        error_sms_code: false,
+        error_sms_code_message: '',
+        sending_flag: false,
+        sms_code_tip: '获取短信验证码',
 
-		uuid: '',
-		image_code_url: '',
-		sms_code_tip: '获取短信验证码',
-		sending_flag: false,
-	},
-	mounted(){
-		// 生成图形验证码
-		this.generate_image_code();
-	},
-	methods: {
-		// 生成图形验证码
-		generate_image_code(){
-			this.uuid = generateUUID();
-			this.image_code_url = "/image_codes/" + this.uuid + "/";
-		},
-		// 检查手机号
-		check_mobile(){
-			let re = /^1[3-9]\d{9}$/;
-			if(re.test(this.mobile)) {
-				this.error_mobile = false;
-			} else {
-				this.error_mobile_message = '您输入的手机号格式不正确';
-				this.error_mobile = true;
-			}
-		},
-		// 检查密码
-		check_password(){
-			let re = /^[0-9A-Za-z]{8,20}$/;
-			if (re.test(this.password)) {
-				this.error_password = false;
-			} else {
-				this.error_password = true;
-			}
-		},
-		// 检查图片验证码
-		check_image_code(){
-			if(!this.image_code) {
-				this.error_image_code_message = '请填写图片验证码';
-				this.error_image_code = true;
-			} else {
-				this.error_image_code = false;
-			}
-		},
-		// 检查短信验证码
-		check_sms_code(){
-			if(!this.sms_code){
-				this.error_sms_code_message = '请填写短信验证码';
-				this.error_sms_code = true;
-			} else {
-				this.error_sms_code = false;
-			}
-		},
-		// 发送短信验证码
-		send_sms_code(){
-			// 避免频繁点击发送短信验证码标签
-			if (this.sending_flag == true) {
-				return;
-			}
-			this.sending_flag = true;
+        error_name: false,
+        error_password: false,
+        error_password2: false,
+        error_mobile: false,
+        error_allow: false,
+        error_submit: false,
 
-			// 校验参数
-			this.check_mobile();
-			this.check_image_code();
-			if (this.error_mobile == true || this.error_image_code == true) {
-				this.sending_flag = false;
-				return;
-			}
-
-			// 向后端接口发送请求，让后端发送短信验证码
-			let url = '/sms_codes/' + this.mobile + '/?image_code=' + this.image_code+'&uuid='+ this.uuid;
-			axios.get(url, {
-				responseType: 'json'
-			})
-				.then(response => {
-					// 表示后端发送短信成功
-					if (response.data.code == '0') {
-						// 倒计时60秒
-						let num = 60;
-						let t = setInterval(() => {
-							if (num == 1) {
-								clearInterval(t);
-								this.sms_code_tip = '获取短信验证码';
-								this.generate_image_code();
-								this.sending_flag = false;
-							} else {
-								num -= 1;
-								this.sms_code_tip = num + '秒';
-							}
-						}, 1000)
-					} else {
-						if (response.data.code == '4001') {
-							this.error_image_code_message = response.data.errmsg;
-							this.error_image_code = true;
-                        } else { // 4002
-							this.error_sms_code_message = response.data.errmsg;
-							this.error_sms_code = true;
-						}
-						this.sending_flag = false;
-					}
-				})
-				.catch(error => {
-					console.log(error.response);
-					this.sending_flag = false;
-				})
-		},
-		// 绑定openid
-		on_submit(){
-			this.check_mobile();
-			this.check_password();
-			this.check_sms_code();
-
-			if(this.error_mobile == true || this.error_password == true || this.error_sms_code == true) {
-				// 不满足条件：禁用表单
-				window.event.returnValue = false
-			}
-		}
-	}
+        error_name_message: '',
+        error_mobile_message: '',
+    },
+    mounted: function () {
+        this.generate_image_code();
+    },
+    methods: {
+        check_username: function () {
+            let re = /^[\w\d_-]{5,20}$/;
+            if (re.test(this.username)) {
+                this.error_name = false;
+            } else {
+                this.error_name = true;
+                this.error_name_message = '请输入5-20个字符的用户名';
+            }
+            let url = '/usernames/' + this.username + '/count/';
+            axios.get(
+                url, {responseType: 'json'}
+            ).then(
+                response => {
+                    if (response.data.count == 1) {
+                        this.error_name_message = '用户名已存在！';
+                        this.error_name = true;
+                    } else {
+                        this.error_name = false;
+                    }
+                }
+            ).catch(
+                error => {
+                    console.log(error.response)
+                }
+            )
+        },
+        check_password: function () {
+            let re = /^[\w\d]{8,20}$/;
+            if (re.test(this.password)) {
+                this.error_password = false;
+            } else {
+                this.error_password = true;
+            }
+        },
+        check_password2: function () {
+            if (this.password2 == this.password2) {
+                this.error_password2 = false;
+            } else {
+                this.error_password2 = true;
+            }
+        },
+        check_mobile: function () {
+            let re = /^1[3-9]\d{9}$/;
+            if (re.test(this.mobile)) {
+                this.error_mobile = false;
+            } else {
+                this.error_mobile = true;
+                this.error_mobile_message = '您输入的手机号格式不正确';
+            }
+            let url = '/mobiles/' + this.mobile + '/count/';
+            axios.get(
+                url, {responseType: 'json'}
+            ).then(
+                response => {
+                    if (response.data.count == 1) {
+                        this.error_mobile_message = '手机号已存在！';
+                        this.error_mobile = true;
+                    } else {
+                        this.error_mobile = false;
+                    }
+                }
+            ).catch(
+                error => {
+                    console.log(error.response)
+                }
+            )
+        },
+        generate_image_code: function () {
+            this.uuid = generateUUID();
+            let url = '/image_codes/' + this.uuid + '/';
+            this.image_code_url = '/image_codes/' + this.uuid + '/';
+        },
+        check_image_code: function () {
+            if (!this.image_code) {
+                this.error_image_code_message = '请填写图片验证码';
+                this.error_image_code = true;
+            } else {
+                let url = '/image_codes/' + this.uuid + '/' + this.image_code + '/';
+                axios.get(
+                    url, {contentType: 'json'}
+                ).then(
+                    response => {
+                        if (response.data.code == '0') {
+                            this.error_image_code = false;
+                        } else {
+                            this.error_image_code_message = '验证码错误！';
+                            this.error_image_code = true;
+                        }
+                    }
+                ).catch(
+                    error => {
+                        console.log(error.response);
+                    }
+                )
+            }
+        },
+        send_sms_code: function () {
+            if (this.sending_flag) {
+                return;
+            }
+            this.sending_flag = true;
+            this.check_mobile();
+            this.check_image_code();
+            if (this.error_mobile || this.error_image_code) {
+                this.sending_flag = false;
+                return;
+            }
+            let url = '/sms_code/' + this.mobile + '/?image_code=' + this.image_code + '&' + 'uuid=' + this.uuid;
+            axios.get(
+                url, {responseType: 'json'}
+            ).then(
+                response => {
+                    if (response.data.code == '0') {
+                        let num = 60;
+                        let t = setInterval(() => {
+                            if (num == 0) {
+                                clearInterval(t);
+                                this.sms_code_tip = '获取短信验证码';
+                                this.sending_flag = false;
+                            } else {
+                                this.sms_code_tip = num + ' 秒';
+                                num -= 1;
+                            }
+                        }, 1000);
+                    } else {
+                        if (response.data.code == '4008') {
+                            this.error_sms_code_message = response.data.errmsg;
+                            this.error_sms_code = true;
+                        } else if (response.data.code == '4001') {
+                            this.error_image_code_message = response.data.errmsg;
+                            this.error_image_code = true;
+                        } else {
+                            this.error_sms_code_message = response.data.errmsg;
+                            this.error_sms_code = true;
+                        }
+                        this.generate_image_code();
+                        this.sending_flag = false;
+                    }
+                }
+            ).catch(
+                error => {
+                    console.log(error.response);
+                    this.sending_flag = false;
+                }
+            )
+        },
+        check_sms_code: function () {
+            let re = /^\d{4}$/;
+            if (!re.test(this.sms_code)) {
+                this.error_sms_code_message = '请填写短信验证码';
+                this.error_sms_code = true;
+            } else {
+                let url = '/sms_code/' + this.mobile + '/' + this.sms_code + '/';
+                axios.get(
+                    url, {responseType: 'json'}
+                ).then(
+                    response => {
+                        if (response.data.code == '0') {
+                            this.error_sms_code = false;
+                        } else {
+                            this.error_sms_code_message = response.data.errmsg;
+                            this.error_sms_code = true;
+                        }
+                    }
+                ).catch(
+                    error => {
+                        console.log(error.response)
+                    }
+                )
+            }
+        },
+        check_allow: function () {
+            if (this.allow) {
+                this.error_allow = false;
+            } else {
+                this.error_allow = true;
+            }
+        },
+        on_submit: function (event) {
+            this.check_username();
+            this.check_password();
+            this.check_password2();
+            this.check_mobile();
+            this.check_allow();
+            if (this.error_name == true || this.error_password == true || this.error_password2 == true
+                || this.error_mobile == true || this.error_allow == true) {
+                this.error_submit = true;
+                event.preventDefault();
+            }
+        },
+    },
 });
