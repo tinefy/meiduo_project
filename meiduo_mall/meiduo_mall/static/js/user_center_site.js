4927c9d2f82c23278be3128558628d5aaf03534e
Tom <tom@ifold.cf>
2022-08-10 20:29:54 +0800

    clear_form_data

--- meiduo_mall/meiduo_mall/static/js/user_center_site.js
+++ meiduo_mall/meiduo_mall/static/js/user_center_site.js
@@ -19,21 +19,29 @@ let vm = new Vue(
             cities: [],
             districts: [],
             is_show_editor: false,
-            error_receiver: false,
-            error_place: false,
-            error_mobile: false,
-            error_tel: false,
-            error_email: false,
-
+            error_tips: {
+                error_receiver: false,
+                error_place: false,
+                error_mobile: false,
+                error_tel: false,
+                error_email: false,
+            },
             editing_address_index: -1,
         },
         methods: {
             clear_form_data: function () {
-                let keys_ = Object.keys(this.form_address)
-                for (let i = 0; i < keys_.length; i++) {
-                    this.form_address[keys_[i]] = '';
+                let keys = Object.keys(this.form_address);
+                for (let i = 0; i < keys.length; i++) {
+                    this.form_address[keys[i]] = '';
                 }
+                this.clear_errors();
                 this.get_areas('province');
+            },
+            clear_errors: function () {
+                let keys = Object.keys(this.error_tips);
+                for (let i = 0; i < keys.length; i++) {
+                    this.error_tips[keys[i]] = '';
+                }
             },
             show_editor: function (e) {
                 e.preventDefault();
@@ -79,41 +87,41 @@ let vm = new Vue(
             check_receiver: function () {
                 let re = /^\s*?$/;
                 if (re.test(this.form_address.receiver)) {
-                    this.error_receiver = true;
+                    this.error_tips.error_receiver = true;
                 } else {
-                    this.error_receiver = false;
+                    this.error_tips.error_receiver = false;
                 }
             },
             check_place: function () {
                 let re = /^\s*?$/;
                 if (re.test(this.form_address.place)) {
-                    this.error_place = true;
+                    this.error_tips.error_place = true;
                 } else {
-                    this.error_place = false;
+                    this.error_tips.error_place = false;
                 }
             },
             check_mobile: function () {
                 let re = /^1[3-9]\d{9}$/;
                 if (!re.test(this.form_address.mobile)) {
-                    this.error_mobile = true;
+                    this.error_tips.error_mobile = true;
                 } else {
-                    this.error_mobile = false;
+                    this.error_tips.error_mobile = false;
                 }
             },
             check_tel: function () {
                 let re = /^(0[0-9]{2,3}-)?([2-9][0-9]{6,7})+(-[0-9]{1,4})?$/;
                 if (!re.test(this.form_address.tel)) {
-                    this.error_tel = true;
+                    this.error_tips.error_tel = true;
                 } else {
-                    this.error_tel = false;
+                    this.error_tips.error_tel = false;
                 }
             },
             check_email: function () {
                 let re = /^[a-z0-9][\w\.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$/;
                 if (!re.test(this.form_address.email)) {
-                    this.error_email = true;
+                    this.error_tips.error_email = true;
                 } else {
-                    this.error_email = false;
+                    this.error_tips.error_email = false;
                 }
             },
             save_address: function (e, index = -1) {
