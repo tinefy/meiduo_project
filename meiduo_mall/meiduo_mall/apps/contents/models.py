5e1fa6184dc28ed2b6818015b25acf4459f4092b
Tom <tom@ifold.cf>
2022-08-25 02:02:53 +0800

    class initialize

--- meiduo_mall/meiduo_mall/apps/contents/models.py
+++ meiduo_mall/meiduo_mall/apps/contents/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
+from meiduo_mall.utils.models import BaseModel
 
 
 # Create your models here.
 class ContentCategory(BaseModel):
+    pass
+
+
+class Content(BaseModel):
     pass
