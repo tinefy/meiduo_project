439d7c780f651ce90968aea0cee8258c4a5c6063
Tom <tom@ifold.cf>
2022-08-25 01:19:39 +0800

    class initialize

--- meiduo_mall/meiduo_mall/apps/goods/models.py
+++ meiduo_mall/meiduo_mall/apps/goods/models.py
@@ -6,3 +6,39 @@ from meiduo_mall.utils.models import BaseModel
 # Create your models here.
 class GoodsCategory(BaseModel):
     pass
+
+
+class GoodsChannelGroup(BaseModel):
+    pass
+
+
+class GoodsChannel(BaseModel):
+    pass
+
+
+class Brand(BaseModel):
+    pass
+
+
+class SPU(BaseModel):
+    pass
+
+
+class SKU(BaseModel):
+    pass
+
+
+class SKUImage(BaseModel):
+    pass
+
+
+class SPUSpecification(BaseModel):
+    pass
+
+
+class SpecificationOption(BaseModel):
+    pass
+
+
+class SKUSpecification(BaseModel):
+    pass
