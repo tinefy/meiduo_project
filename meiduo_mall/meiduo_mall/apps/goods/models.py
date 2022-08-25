54932ec8fb5bee2f61b280d73f27766fc3721558
Tom <tom@ifold.cf>
2022-08-26 01:50:05 +0800

    class model initialize

--- meiduo_mall/meiduo_mall/apps/goods/models.py
+++ meiduo_mall/meiduo_mall/apps/goods/models.py
@@ -17,3 +17,29 @@ class GoodsCategory(BaseModel):
     def __str__(self):
         return self.name
 
+
+class GoodsChannelGroup(BaseModel):
+    name = models.CharField(max_length=20, verbose_name='频道组名')
+
+    class Meta:
+        db_table = 'tb_channel_group'
+        verbose_name = '商品频道组'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+class GoodsChannel(BaseModel):
+    group = models.ForeignKey(GoodsChannelGroup, verbose_name='频道组名')
+    category = models.ForeignKey(GoodsCategory, on_delete=models.CASCADE, verbose_name='顶级商品类别')
+    url = models.CharField(max_length=50, verbose_name='频道页面链接')
+    sequence = models.IntegerField(verbose_name='组内顺序')
+
+    class Meta:
+        db_table = 'tb_goods_channel'
+        verbose_name = '商品频道'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.category.name
