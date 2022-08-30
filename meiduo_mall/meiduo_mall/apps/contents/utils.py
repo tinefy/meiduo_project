bcbd3b99efe71db1180b3b50a9eb0a259964192e
Tom <tom@ifold.cf>
2022-08-30 10:58:47 +0800

    IndexView categories OK

---
+++ meiduo_mall/meiduo_mall/apps/contents/utils.py
@@ -0,0 +1,40 @@
+from goods.models import GoodsChannel
+
+
+def get_categories():
+    # categories = OrderedDict()
+    categories = dict()
+    channels = GoodsChannel.objects.order_by('group_id', 'sequence')
+    for channel in channels:
+        group_id = channel.group_id
+        if group_id not in categories:
+            categories[group_id] = {
+                'channels': [],
+                'sub_cats': [],
+            }
+        category_in_level_1 = channel.category
+        categories[group_id]['channels'].append(
+            {
+                'id': category_in_level_1.id,
+                'name': category_in_level_1.name,
+                'url': channel.url,
+            }
+        )
+        # for category_in_level_2 in category_in_level_1.subs.all():
+        for category_in_level_2 in category_in_level_1.goodscategory_set.all():
+            category_in_level_2_sub_cats = []
+            for category_in_level_3 in category_in_level_2.goodscategory_set.all():
+                category_in_level_2_sub_cats.append(
+                    {
+                        'id': category_in_level_3.id,
+                        'name': category_in_level_3.name
+                    }
+                )
+            categories[group_id]['sub_cats'].append(
+                {
+                    "id": category_in_level_2.id,
+                    "name": category_in_level_2.name,
+                    "sub_cats": category_in_level_2_sub_cats
+                }
+            )
+    return categories
--- meiduo_mall/meiduo_mall/apps/contents/views.py
+++ meiduo_mall/meiduo_mall/apps/contents/views.py
@@ -1,48 +1,15 @@
-from collections import OrderedDict
-
 from django.shortcuts import render
 from django.views import View
 
 # Create your views here.
-from goods.models import GoodsChannel
+from .utils import get_categories
 
 
 class IndexView(View):
+
     def get(self, request):
-        # categories = OrderedDict()
-        categories = dict()
-        channels = GoodsChannel.objects.order_by('group_id', 'sequence')
-        for channel in channels:
-            group_id = channel.group_id
-            if group_id not in categories:
-                categories[group_id] = {
-                    'channels': [],
-                    'sub_cats': [],
-                }
-            category_in_level_1 = channel.category
-            categories[group_id]['channels'].append(
-                {
-                    'id': category_in_level_1.id,
-                    'name': category_in_level_1.name,
-                    'url': channel.url,
-                }
-            )
-            # for category_in_level_2 in category_in_level_1.subs.all():
-            for category_in_level_2 in category_in_level_1.goodscategory_set.all():
-                category_in_level_2_sub_cats = []
-                for category_in_level_3 in category_in_level_2.goodscategory_set.all():
-                    category_in_level_2_sub_cats.append(
-                        {
-                            'id': category_in_level_3.id,
-                            'name': category_in_level_3.name
-                        }
-                    )
-                categories[group_id]['sub_cats'].append(
-                    {
-                        "id": category_in_level_2.id,
-                        "name": category_in_level_2.name,
-                        "sub_cats": category_in_level_2_sub_cats
-                    }
-                )
-        print(categories)
-        return render(request, 'index.html')
+        categories = get_categories()
+        context = {
+            'categories': categories
+        }
+        return render(request, 'index.html', context=context)
