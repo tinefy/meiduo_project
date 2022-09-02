9fe3777a95181016bb324c4baa79b87c0ad809c6
Tom <tom@ifold.cf>
2022-09-03 04:19:21 +0800

    '# '

--- meiduo_mall/meiduo_mall/static/js/index.js
+++ meiduo_mall/meiduo_mall/static/js/index.js
@@ -1,9 +1,24 @@
-let vm=new Vue(
+let vm = new Vue(
     {
-        el:'#app',
-        delimiters:['[[',']]'],
-        data:{
-            username:getCookie('username'),
+        el: '#app',
+        delimiters: ['[[', ']]'],
+        data: {
+            username: getCookie('username'),
+
+            f1_tabs: [
+                '时尚新品',
+                '畅想低价',
+                '手机配件',
+            ],
+
+            f1_tab: 1,
+            f2_tab: 1,
+            f3_tab: 1,
+        },
+        methods: {
+            floor_tab: function (tab_in_floor, index) {
+                tab_in_floor = index + 1;
+            },
         }
     }
 )
