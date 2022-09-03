59eedff25c98fde9c7f9e02b92023149254d7246
Tom <tom@ifold.cf>
2022-09-03 12:52:03 +0800

    展示首页商品广告

--- meiduo_mall/meiduo_mall/static/js/index.js
+++ meiduo_mall/meiduo_mall/static/js/index.js
@@ -5,19 +5,38 @@ let vm = new Vue(
         data: {
             username: getCookie('username'),
 
-            f1_tabs: [
-                '时尚新品',
-                '畅想低价',
-                '手机配件',
-            ],
+            // f1_tabs: [
+            //     '时尚新品',
+            //     '畅想低价',
+            //     '手机配件',
+            // ],
+            //
+            // f1_tab: 1,
+            // f2_tab: 1,
+            // f3_tab: 1,
 
-            f1_tab: 1,
-            f2_tab: 1,
-            f3_tab: 1,
+            floors: {
+                f1_tabs: [
+                    '时尚新品',
+                    '畅想低价',
+                    '手机配件',
+                ],
+                f2_tabs: [
+                    '加价换购',
+                    '畅享低价',
+                ],
+                f3_tabs: [
+                    '生活用品',
+                    '厨房用品',
+                ],
+                f1_tab: 1,
+                f2_tab: 1,
+                f3_tab: 1,
+            },
         },
         methods: {
-            floor_tab: function (tab_in_floor, index) {
-                tab_in_floor = index + 1;
+            floor_tab: function (floor, index) {
+                this.floors['f' + floor + '_tab'] = index + 1;
             },
         }
     }
