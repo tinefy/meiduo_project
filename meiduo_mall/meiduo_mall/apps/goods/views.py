5fc3659e29992710bc7e3fc87c3ac0209946d892
Tom <tom@ifold.cf>
2022-09-13 04:57:49 +0800

    page_skus

--- meiduo_mall/meiduo_mall/apps/goods/views.py
+++ meiduo_mall/meiduo_mall/apps/goods/views.py
@@ -1,12 +1,14 @@
-from django.http import HttpResponseNotFound
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponseNotFound
+from django.core.paginator import Paginator
 
 # Create your views here.
 from contents.utils import get_categories
 
-from .models import GoodsCategory
+from .models import GoodsCategory, SKU
 from .utils import get_breadcrumb
+from . import constants
 
 
 class ListView(View):
@@ -15,12 +17,37 @@ class ListView(View):
             category = GoodsCategory.objects.get(id=category_id)
         except GoodsCategory.DoesNotExist:
             return HttpResponseNotFound('GoodsCategory does not exist')
+        # 接收sort参数：默认=default
+        sort = request.GET.get('sort', 'default')
+        # 按照排序规则查询该分类商品SKU信息
+        if sort == 'price':
+            sort_field = 'price'
+        elif sort == 'hot':
+            sort_field = '-sales'
+        else:
+            sort = 'default'
+            sort_field = 'create_time'
+        skus = SKU.objects.filter(category=category, is_launched=True).order_by(sort_field)
+        # 创建分页器：每页N条记录
+        paginator = Paginator(skus, constants.GOODS_LIST_LIMIT)
+        # 获取列表页总页数
+        total_page = paginator.num_pages
+        # 获取当前页面sku数据
+        if total_page >= int(page_num) >= 1:
+            page_skus = paginator.page(page_num)
+        else:
+            return HttpResponseNotFound('page is empty')
         # 查询商品频道和分类
         categories = get_categories()
         # 查询面包屑导航
         breadcrumb = get_breadcrumb(category)
         context = {
-            'categories': categories,
-            'breadcrumb': breadcrumb,
+            'categories': categories,  # 频道分类
+            'breadcrumb': breadcrumb,  # 面包屑导航
+            'sort': sort,  # 排序字段
+            'category': category,
+            'page_skus': page_skus,  # 当前页面sku数据
+            'total_page': total_page,  # 总页数
+            'page_num': page_num,  # 当前页码
         }
         return render(request, 'list.html', context=context)
