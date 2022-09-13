42b05fa687a82ad25a6d970485a2e545ec1eba94
Tom <tom@ifold.cf>
2022-09-13 10:42:25 +0800

    列表页 Done

--- meiduo_mall/meiduo_mall/static/js/list.js
+++ meiduo_mall/meiduo_mall/static/js/list.js
@@ -4,12 +4,33 @@ let vm = new Vue(
         delimiters: ['[[', ']]'],
         data: {
             username: getCookie('username'),
-        },
-        methods:{
-            
-        },
-        mounted:function () {
 
-        }
+            category_id: category_id,
+            hot_skus: [],
+        },
+        methods: {
+            get_hot_skus: function () {
+                if (this.category_id) {
+                    let url = '/list/hot/' + this.category_id + '/';
+                    axios.get(
+                        url, {responseType: 'json'}
+                    ).then(
+                        response => {
+                            this.hot_skus = response.data.hot_skus;
+                            for (let i = 0; i < this.hot_skus.length; i++) {
+                                this.hot_skus[i].url = '/detail/' + this.hot_skus[i].id + '/';
+                            }
+                        }
+                    ).catch(
+                        error => {
+                            console.log(error.response);
+                        }
+                    )
+                }
+            },
+        },
+        mounted: function () {
+            this.get_hot_skus();
+        },
     }
 )
