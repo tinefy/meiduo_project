a389f422f193b78e1afdd1edb466f8867b0e1942
Tom <tom@ifold.cf>
2022-09-19 03:03:14 +0800

    商品详情页

--- meiduo_mall/meiduo_mall/static/js/detail.js
+++ meiduo_mall/meiduo_mall/static/js/detail.js
@@ -7,9 +7,27 @@ let vm = new Vue(
 
             category_id: category_id,
             sku_price: sku_price,
+            sku_count: 1,
+
         },
         methods: {
-
+            check_sku_count: function () {
+                if (this.sku_count > 5) {
+                    this.sku_count = 5;
+                } else if (this.sku_count < 1) {
+                    this.sku_count = 1;
+                }
+            },
+            sku_count_add: function () {
+                if (this.sku_count < 5) {
+                    this.sku_count++;
+                }
+            },
+            sku_count_minus: function () {
+                if (this.sku_count > 1) {
+                    this.sku_count--;
+                }
+            },
         },
     }
 )
