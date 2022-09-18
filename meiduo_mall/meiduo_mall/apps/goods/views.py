55a122aa952cae1a70086e3528f55483260188dd
Tom <tom@ifold.cf>
2022-09-18 19:52:15 +0800

    商品详情页

--- meiduo_mall/meiduo_mall/apps/goods/views.py
+++ meiduo_mall/meiduo_mall/apps/goods/views.py
@@ -88,47 +88,57 @@ class DetailView(View):
         # 查询面包屑导航
         breadcrumb = get_breadcrumb(category)
 
-        # 当前商品的所有规格 <QuerySet [<SKUSpecification: 1: Apple MacBook Pro 13.3英寸笔记本 银色: 屏幕尺寸 - 13.3英寸>, ...]>
-        sku_specs = sku.skuspecification_set.order_by('spec_id')
-        # 构建当前商品的规格选项键，列表的每一项为规格选项信息(例如 ”13.3英寸“)的id
-        sku_specs_key = []
-        for sku_spec in sku_specs:
-            sku_specs_key.append(sku_spec.option.id)  # SpecificationOption
+        # 当前商品的所有sku_spec按spu_spec排序
+        sku_skuspecs = sku.skuspecification_set.order_by('spec_id')
+        # 当前商品的sku规格选项id列表,index为spu_spec的顺序
+        sku_skuspec_option_ids_list = []
+        for sku_skuspec in sku_skuspecs:
+            sku_skuspec_option_ids_list.append(sku_skuspec.option.id)  # SpecificationOption
 
-        # 获取当前商品的所有SKU
-        skus = sku.spu.sku_set.all()
-        # 构建所有sku字典，每一个sku id(value)对应所有的规格选项信息的id(key) spec_sku_map {(1, 4, 7): 1, (1, 3, 7): 2}
-        spec_sku_map = {}
-        for sku_item in skus:
+        # 获取当前商品同一spu下的的所有sku
+        all_skus = sku.spu.sku_set.all()
+        # 字典，每一个sku_id(value)对应所有sku_spec选项信息的id(key) {(1, 4, 7): 1, (1, 3, 7): 2}
+        dict_k_skuspec_option_ids_v_sku_id = {}
+        for every_sku in all_skus:
             # 每一个SKU的所有规格
-            sku_item_specs = sku_item.skuspecification_set.order_by('spec_id')
+            every_sku_skuspecs = every_sku.skuspecification_set.order_by('spec_id')
             # 每一个SKU的规格选项键，列表的每一项为规格选项信息的id
-            sku_item_spec_key = []
-            for sku_item_spec in sku_item_specs:
-                sku_item_spec_key.append(sku_item_spec.option.id)
+            every_sku_skuspecs_option_ids_list = []
+            for every_sku_skuspec in every_sku_skuspecs:
+                every_sku_skuspecs_option_ids_list.append(every_sku_skuspec.option.id)
             # key:每一个SKU的所有规格选项信息的id value:每一个SKU的id
-            spec_sku_map[tuple(sku_item_spec_key)] = sku_item.id
+            dict_k_skuspec_option_ids_v_sku_id[tuple(every_sku_skuspecs_option_ids_list)] = every_sku.id
 
-        # 获取当前商品的规格选项(例如 ”屏幕尺寸“) <QuerySet [<SPUSpecification: Apple MacBook Pro 笔记本: 屏幕尺寸>,...]>
-        sku_spu_specs = sku.spu.spuspecification_set.order_by('id')
-        # 若当前sku的规格选项少于spu规格选项，则信息不完整，则不再继续
-        if len(sku_specs_key) < len(sku_spu_specs):
-            return HttpResponseNotFound('当前商品sku的规格信息不完整')
-        for index, sku_spu_spec in enumerate(sku_spu_specs):
-            # key = sku_spec_key.copy()
-            key = sku_specs_key[:]
-            # 该规格选项的所有选项信息
-            sku_spu_spec_options = sku_spu_spec.specificationoption_set.all()
-            for sku_spu_spec_option in sku_spu_spec_options:
-                key[index] = sku_spu_spec_option.id
+        # 当前商品spu的规格
+        spu_spuspecs = sku.spu.spuspecification_set.order_by('id')
+        # 若当前sku的规格选项少于spu规格，则某些spu规格无选项，信息不全
+        if len(sku_skuspec_option_ids_list) < len(spu_spuspecs):
+            return HttpResponseNotFound('当前商品sku的规格选项不完整')
+        # index为spu_specs的顺序，与sku_skuspec_option_ids_list的index一致
+        for index, every_spuspec in enumerate(spu_spuspecs):
+            # 当前商品的sku规格选项id列表 key = sku_spec_key.copy()
+            key = sku_skuspec_option_ids_list[:]
+            # 每一spu_spec的所有选项值
+            every_spuspec_options = every_spuspec.specificationoption_set.all()
+            #  每一spu_spec的所有选项中的每一个选项值
+            for every_spuspec_option in every_spuspec_options:
+                # 每一个选项值的id, 赋值给当前商品的sku规格选项id列表中，每一spu_spec的index所对应的值
+                key[index] = every_spuspec_options.id
                 # dict.get(a) a不存在时返回None,dict[a] a不存在时报错
-                sku_spu_spec_option.sku_id_added_attr = spec_sku_map.get(tuple(key))
-            sku_spu_spec.spec_options_added_attr = sku_spu_spec_options
+                # 在字典中查询新的sku规格选项id列
+                # 若存在，则返回sku_id，若不存在，则返回None
+                # 给每一spu_spec的所有选项值中的每一个选项值对象新建的属性sku_id_added_attr
+                # 这样每一个选项值，都有一个属性sku_id_added_attr，属性值为sku_id的，为当前商品拥有的选项值
+                # 属性值为None的，不为当前商品拥有的选项值
+                every_spuspec_option.sku_id_added_attr = dict_k_skuspec_option_ids_v_sku_id.get(tuple(key))
+            # 每一spu_spec的所有选项值，赋值给每一spu_spec对象新建的属性spuspec_options_added_attr
+            # 每一spu_spec的所有选项值中的每一选项值都有属性sku_id_added_attr，表明是否为当前商品拥有的选项值
+            every_spuspec.spuspec_options_added_attr = every_spuspec_options
 
         context = {
             'categories': categories,  # 频道分类
             'breadcrumb': breadcrumb,  # 面包屑导航
             'sku': sku,
-            'spec': sku_spu_spec,
+            'spec': spu_spuspecs,
         }
         return render(request, 'detail.html', context=context)
